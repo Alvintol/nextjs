74cd0865d2d97aead0787207755db23a6d0cf4ba
Alvin <alvin.tol@hotmail.com>
2022-09-24 11:04:57 -0600

    imports and wraps Layout into app component

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -1,7 +1,12 @@
+
+import Layout from '../components/layout'
 import '../styles/globals.css'
 
-const MyApp = ({ Component, pageProps }) =>
-  <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 
 
 export default MyApp
