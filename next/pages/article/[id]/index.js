06ee1b37320a30e721dbef8b63b12828100fab76
Alvin <alvin.tol@hotmail.com>
2022-09-24 15:28:16 -0600

    adds getServerSideProps to [id] page to retrieve data instead of router

--- next/pages/article/[id]/index.js
+++ next/pages/article/[id]/index.js
@@ -1,13 +1,28 @@
 import { useRouter } from 'next/dist/client/router';
 
 
-const article = () => {
-  const router = useRouter();
-  const {id} = router.query;
+const article = ({ article }) => {
+  // const router = useRouter();
+  // const {id} = router.query;
+
+  const { id } = article;
 
   return (
-    <div>This is article #{id} </div>
+    <>
+    This is article #{id}
+    </>
   )
 };
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+  const article = await res.json();
+
+  return {
+    props: {
+      article
+    }
+  }
+}
 
 export default article;
