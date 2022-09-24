39b431f8f1b6975d7440fc314c146013557369fe
Alvin <alvin.tol@hotmail.com>
2022-09-24 14:56:31 -0600

    adds getStaticProps varible

--- next/pages/index.js
+++ next/pages/index.js
@@ -10,5 +10,17 @@ const Home = () => {
       <h1>Welcome to Next</h1>
     </div>
   )
-}
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6');
+  const articles = await res.jason();
+
+  return {
+    props: {
+      articles
+    }
+  }
+};
+
 export default Home;
