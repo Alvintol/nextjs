1f585834c526f26de92c37d1ef13379ec4f5dbb4
Alvin <alvin.tol@hotmail.com>
2022-09-24 10:54:44 -0600

    adds Head component, titla ena meta tag in home component

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
+
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>Webdev Newz</title>
+        <meta name='keywords' content='web development, programming' />
+      </Head>
       <h1>Welcome to Next</h1>
     </div>
   )
 }
-
 export default Home;
