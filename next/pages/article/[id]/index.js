577d4e9615a7993239757536888ae531145e51ad
Alvin <alvin.tol@hotmail.com>
2022-09-24 15:24:14 -0600

    imports useRouter to display article ID in [id] page

--- next/pages/article/[id]/index.js
+++ next/pages/article/[id]/index.js
@@ -1,7 +1,12 @@
+import { useRouter } from 'next/dist/client/router';
+
 
 const article = () => {
+  const router = useRouter();
+  const {id} = router.query;
+
   return (
-    <div>This is an article</div>
+    <div>This is article #{id} </div>
   )
 };
 
