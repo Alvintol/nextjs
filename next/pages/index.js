71bed9f44ce33fb0c2bab2dc0c4f5791732c43b1
Alvin <alvin.tol@hotmail.com>
2022-09-24 10:52:38 -0600

    adds about page

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export const Home = () => {
+const Home = () => {
   return (
-    <div >
-      
+    <div>
+      <h1>Welcome to Next</h1>
     </div>
   )
 }
+
+export default Home;
