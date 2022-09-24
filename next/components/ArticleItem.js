e0205670adda9fe46e38465b6da4d1141a02b0a4
Alvin <alvin.tol@hotmail.com>
2022-09-24 15:18:18 -0600

    displays article title, body and respective link

--- next/components/ArticleItem.js
+++ next/components/ArticleItem.js
@@ -1,11 +1,18 @@
 import articleStyles from '../styles/Article.module.css';
 import Link from 'next/link';
 
-const ArticleItem = () => {
-  return (
-    <div>
+const ArticleItem = ({ article }) => {
 
-    </div>
+  const { card } = articleStyles;
+  const { id, title, body } = article;
+  
+  return (
+    <Link href='/article/[id]' as={`/article/${id}`}>
+      <a className={card}>
+        <h3>{title} &rarr;</h3>
+        <p>{body}</p>
+      </a>
+    </Link>
   )
 };
 
