b84baaf3aaaeef3f9810cc13859089fb278bf203
Alvin <alvin.tol@hotmail.com>
2022-09-24 15:05:38 -0600

    displayed article titles

--- next/pages/index.js
+++ next/pages/index.js
@@ -7,14 +7,16 @@ const Home = ({ articles }) => {
         <title>Webdev Newz</title>
         <meta name='keywords' content='web development, programming' />
       </Head>
-      <h1>Welcome to Next</h1>
+      {articles.map(article =>
+        <h3>{article.title}</h3>
+      )}
     </div>
   )
 };
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6');
-  const articles = await res.jason();
+  const articles = await res.json();
 
   return {
     props: {
