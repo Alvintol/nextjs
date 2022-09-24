5e7ee546c6a90b9eccb180a8cb5005b491da6358
Alvin <alvin.tol@hotmail.com>
2022-09-24 16:13:52 -0600

    adds single data route

---
+++ next/pages/api/articles/[id].js
@@ -0,0 +1,11 @@
+import { articles } from '../../../data';
+
+const handler = ({ query: { id } }, res) => {
+  const filtered = articles.filter(article => article.id === id);
+
+  filtered.length > 0 ?
+    res.status(200).json(filtered[0]) :
+    res.status(404).json({ message: `Article #${id} not found` })
+}
+
+export default handler;
