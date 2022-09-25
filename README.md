c15593dcc26e3126ea0a57530e663a6dcddb15e6
Alvin <alvin.tol@hotmail.com>
2022-09-24 21:33:22 -0600

    removes head folder

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nextjs-fundamentals
+Built a simple data fetching app using NextJS to cover fundamentals such as SSR & SSG, routing, data fetching
--- pages/api/articles/[id].js
+++ pages/api/articles/[id].js
@@ -3,7 +3,7 @@ import { articles } from '../../../data';
 const handler = ({ query: { id } }, res) => {
   const filtered = articles.filter(article => article.id === id);
 
-  filtered.length > 0 ?
+  return filtered.length > 0 ?
     res.status(200).json(filtered[0]) :
     res.status(404).json({ message: `Article #${id} not found` })
 }
